a457925ae5e44d800dba0f13f20037d94cd359b3
ElsayedmM <mohammad.elsayedm@gmail.com>
2019-03-24 06:17:29 +0200

    what i did at sayed

--- app.js
+++ app.js
@@ -6,24 +6,21 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 2888
 const msgSchema = {
-    room: Joi.number().required(),
-    card_id: Joi.number().min(-1).max(360).required(),
+    roomId: Joi.string().required(),
+    playerId: Joi.string().required(),
+    cardId: Joi.number().min(-1).max(360).required(),
 }
-const rooms = {
-    "roomID": {
-        roomSize: 1,
-        players: { "playerID": responseObject }
-    }
-}
+const rooms = {}
 const joint_match = []
 const MAX = 4
 const MIN = 3
+const DELAY_BEFORE_MATCH = 20 * 1000
 let timerAlreadyStarted = false
 let timer
 
 /**================================================================================================= */
 
-function getRandomIn(dic) {
+function getNewRandomStringFor(dic) {
     let r
     do {
         r = Math.random().toString(36).substring(2);
@@ -34,21 +31,41 @@ function getRandomIn(dic) {
 function createRoom(size) {
     return {
         roomSize: size,
+        judgeIndex: 0,
         players: {}
     }
 }
 
+function getRandomCard() { return Math.random()  * 360 }
+
 function sendToAllMatchStarted() {
-    let rid = getRandomIn(rooms)
+    let rid = getNewRandomStringFor(rooms)
     rooms[rid] = createRoom(joint_match.length)
 
     joint_match.forEach((res, i) => {
-        let pid = getRandomIn(rooms[rid].players)
-        res.send({ "room": rid, "playerID": pid })
+        let pid = getNewRandomStringFor(rooms[rid].players)
+        res.send({
+            "roomId": rid,
+            "playerId": pid,
+            "isJudge": i==rooms[rid].judgeIndex,
+            "cardId": getRandomCard()
+          })
         rooms[rid].players[pid] = null
-        //TODO: choose a judge
     });
     joint_match.length = 0
+}
+
+function sendToJudge(roomId, cardId) {
+    const thePlayers = rooms[roomId].players
+    for(const p in thePlayers){
+        if(p!= rooms[roomId].j&& thePlayers[p]){
+            thePlayers[p].send()
+        }
+    }
+}
+
+function sendToAllExceptJudge(roomId,cardId) {
+    
 }
 
 /**================================================================================================= */
@@ -64,7 +81,7 @@ app.get('/', (request, response) => {
     }
     joint_match.push(response)
     if (joint_match.length >= MIN && !timerAlreadyStarted) {
-        timer = setTimeout(sendToAllMatchStarted, 30000);// 30 sec
+        timer = setTimeout(sendToAllMatchStarted, DELAY_BEFORE_MATCH);
         timerAlreadyStarted = true
     }
 })
@@ -79,5 +96,15 @@ app.get('/friends', (request, response) => {
 app.post('/', (request, response) => {
     const result = Joi.validate(request.body, msgSchema)
     if (result.error) return response.status(400).send(result.error)
-    response.send(result)
+    let roomId = request.body.room
+    let playerId = request.body.playerId
+    if(roomId in rooms && playerId in rooms[roomId].players){
+        if(Object.keys(rooms[roomId].players).indexOf(playerId) == rooms[roomId].judgeIndex){
+            // TODO send to all except the judge the selected cardId
+        }else{
+            // TODO send that card to the judge to choose
+        }
+    }else{
+        response.status(406).send(result)
+    }
 })
